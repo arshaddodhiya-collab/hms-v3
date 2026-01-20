aea2c8f8f30be81c9c91f000b0464a70ae186ccc
arshaddodhiya-collab <arshad.dodhiya@artemhealthtech.com>
2026-01-20 13:23:18 +0530

    services path change

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from '../auth/auth.service';
+import { AuthService } from '../../auth/auth.service';
 import { MessageService } from 'primeng/api';
 
 @Component({
