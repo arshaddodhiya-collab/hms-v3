ec74f8a9f7f200ea79a5143bb39fa5b47fd77ee5
arshaddodhiya-collab <arshad.dodhiya@artemhealthtech.com>
2026-01-23 11:29:55 +0530

    removing old version for rebuild

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,55 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { authGuard } from './auth/auth.guard';
-import { roleGuard } from './auth/role.guard';
 
-const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  {
-    path: 'login',
-    loadChildren: () =>
-      import('./features/login/login.module').then((m) => m.LoginModule),
-  },
-  {
-    path: 'admin',
-    loadChildren: () =>
-      import('./features/admin/admin.module').then((m) => m.AdminModule),
-    canActivate: [authGuard, roleGuard],
-    data: { role: 'ADMIN' },
-  },
-  {
-    path: 'reception',
-    loadChildren: () =>
-      import('./features/front-desk/front-desk.module').then(
-        (m) => m.FrontDeskModule,
-      ),
-    canActivate: [authGuard, roleGuard],
-    data: { role: 'FRONT DESK' },
-  },
-  {
-    path: 'doctor',
-    loadChildren: () =>
-      import('./features/doctor/doctor.module').then((m) => m.DoctorModule),
-    canActivate: [authGuard, roleGuard],
-    data: { role: 'DOCTOR' },
-  },
-  {
-    path: 'nurse',
-    loadChildren: () =>
-      import('./features/nurse/nurse.module').then((m) => m.NurseModule),
-    canActivate: [authGuard, roleGuard],
-    data: { role: 'NURSE' },
-  },
-  {
-    path: 'lab',
-    loadChildren: () =>
-      import('./features/lab/lab.module').then((m) => m.LabModule),
-    canActivate: [authGuard, roleGuard],
-    data: { role: 'LAB' },
-  },
-  // Fallback
-  { path: '**', redirectTo: 'login' },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
